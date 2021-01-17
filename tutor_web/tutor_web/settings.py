d1c06b917a050605b98a2577c2f77dbc1167966f
ReznikovRoman <romanreznikov2002@yandex.ru>
2021-01-17 22:55:20 +0300

    Start 'accounts' app

--- tutor_web/tutor_web/settings.py
+++ tutor_web/tutor_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!3ksu+oa2r8nei_vi&j((6@d*p&)p^=7yoekcb-$c0blv&wmm-'
+SECRET_KEY = os.environ.get('DJANGO_TUTOR_WEB_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
